e8aebb3a38ff6a57eea2bc77b3830e3eb81abf46
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-09-12 09:59:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Comments from './Comments';
-import CommentForm from './CommentForm';
+import CommentsForm from './CommentForm';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden : true
+    }
+    this.handleShowForm = this.handleShowForm.bind(this);
+  }
+
+  handleShowForm(){
+    this.setState((prevState)=>{
+      return {
+        hidden : !prevState.hidden
+      }
+    })
+  }
+
+  render(){
     return (
-      <div className="container">
-        <CommentForm/>
-        <Comments/>
+      <div className='container'>
+          <button className="button" onClick={this.handleShowForm}>Add Comment</button>
+          <CommentsForm hidden={this.state.hidden} onSubmit={this.handleShowForm}/>
+          <Comments/>
       </div>
-      
-    );
+      )
   }
 }
 
